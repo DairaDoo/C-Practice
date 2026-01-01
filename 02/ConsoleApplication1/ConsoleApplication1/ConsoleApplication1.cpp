818e68c85e1f53f93dda4e62c075cdda1b6cbb13
DairanDoo <your_email@example.com>
2026-01-01 18:27:03 -0400

    using local and global scope

---
+++ 02/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int count = 10; // global variable
+
+int main()
+{
+
+	int count = 20; // local, sobreescribe la global.
+
+	cout << "Local count: " << count << endl;
+	cout << "Global count: " << ::count << endl;
+
+	return 0;
+}
+
+
+//void PointersExercise() {
+//	int* pPtr = new int(10);
+//
+//	cout << "Dirección: " << pPtr << endl;
+//
+//	cout << "Valor en memoria:" << *pPtr;
+//
+//	delete(pPtr);
+//}
+//
